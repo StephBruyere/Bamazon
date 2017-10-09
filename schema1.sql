7f743c761a416cfbd51b919d7e7d267281d5fb6a
Steph Bruyere <sjones11486@yahoo.com>
2017-10-08 21:24:01 -0400

    new database

---
+++ schema1.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS	bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE bamazon (
+item_number int NOT NULL,
+product_name VARCHAR(100) NOT NULL,
+department_name VARCHAR(100) NOT NULL,
+price int NOT NULL,
+stock int NOT NULL,
+PRIMARY KEY (item_number)
+);
+
+SELECT * FROM bamazon;
+
+show tables;
+
+
+
+
