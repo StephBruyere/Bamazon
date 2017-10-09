9f9c62ed8f122d72b86d6caccfc1b4d667da57f3
Steph Bruyere <sjones11486@yahoo.com>
2017-10-08 22:50:03 -0400

    initial table done

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,16 +1,26 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  // Your username
-  user: "root",
-  // Your password
-  password: "3vil1337",
-  database: "bamazon"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "3vil1337",
+    database: "bamazon"
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.price);
+    if (err) throw err;
+    showProducts();
+    connection.end();
 });
+
+function showProducts() {
+    var query = "SELECT * FROM product";
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log("Product ID: " + res[i].item_number + "Product Name: " +
+                res[i].product_name + " Price: " + res[i].price);
+        }
+    })
+}
