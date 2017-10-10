f4629e570bba1816224d13703881649ce2dc8fbc
Steph Bruyere <sjones11486@yahoo.com>
2017-10-09 22:58:40 -0400

    completed customer file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,7 +11,6 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     showProducts();
-    connection.end();
 });
 
 function showProducts() {
@@ -25,5 +24,70 @@ function showProducts() {
             console.log("       " + res[i].item_number + "              " + res[i].product_name + "              " + res[i].department_name + "                 " + res[i].price + "              " + res[i].stock);
             console.log("                                                                                          ");
         }
-    })
+        start();
+    });
+}
+
+function start() {
+    inquirer
+        .prompt([{
+            name: "productID",
+            type: "input",
+            message: "Select a Product ID:",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                console.log(" <YOU MUST ENTER A QUANTITY>")
+                return false;
+            }
+        }, {
+            name: "total",
+            type: "input",
+            message: "Indicate the quantity you wish to purchase:",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                console.log(" <YOU MUST ENTER A QUANTITY>")
+                return false;
+            }
+        }])
+        .then(function(answer) {
+            var query = "SELECT item_number, product_name, department_name, price, stock FROM product";
+            connection.query(query, { item_number: answer.total, stock: answer.PriceID }, function(err, res) {
+                var name = res[0].product_name;
+                var stock = res[0].stock;
+                var price = res[0].price;
+                var updatedStock = parseInt(stock) - parseInt(answer.total);
+                var updatedPrice = price++;
+                console.log("You have purchased " + answer.total + name + " for a total of $" + answer.total * price);
+
+                function dataChange() {
+                    var query = "UPDATE product SET ? WHERE ?";
+                    connection.query(query, [{
+                        stock: updatedStock
+                    }, {
+                        price: updatedPrice
+                    }])
+                }
+                dataChange();
+                console.log("There are " + updatedStock + name + " available");
+                if (stock[0] <= 0) { console.log("Insufficient Quantity!") }
+                restart();
+            })
+        })
+}
+
+function restart() {
+    inquirer.prompt([{
+        name: "restart",
+        message: "\n Would you like to order again?",
+        type: "list",
+        choices: ["Order Again?", "Quit?"]
+    }]).then(function(answers) {
+        if (answers.restart === "Order Again?") {
+            start();
+        } else { console.log("\nThanks for your order!"); }
+    });
 }
